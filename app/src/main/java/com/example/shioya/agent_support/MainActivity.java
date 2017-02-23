2fe94a8036a58d85ea662f1dbbe51c08c002e1c1
Masataka Shioya <almond776@gmail.com>
2017-02-24 03:50:57 +0900

    エージェント画像と解説テキスト画像の表示

--- app/src/main/java/com/example/shioya/agent_support/MainActivity.java
+++ app/src/main/java/com/example/shioya/agent_support/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.shioya.agent_support;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton imageButton1 = (ImageButton)findViewById(R.id.detectiveButton);
+        imageButton1.setImageResource(R.drawable.nightmare);
+
+        ImageView imageView1 = (ImageView)findViewById(R.id.detectiveText);
+        imageView1.setImageResource(R.drawable.nightmare_text);
     }
 }
